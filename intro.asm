74b7b2f6fb40b958631de1a22036e5e3a1df2bd9
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2021-08-11 22:51:17 +0200

    Moving sprite to level1, starting sprite frame management on move

--- intro.asm
+++ intro.asm
@@ -16,82 +16,11 @@ Intro: {
       jsr Init
       jsr AddColorToMap
 
-    JoystickMovement:
-      jsr WaitRoutine
+    CheckFire:
       jsr GetJoystickMove
 
-      lda Direction
-      beq CheckDirectionY
-
-// Handle horizontal move
-      cmp #$ff
-      beq MoveToLeft
-
-    MoveToRight:
-//        jsr switch_sprite_frog
-      ldx SPRITES.X0          // Moving to right
-      inx                     // Calculate new sprite x position
-      beq ToggleExtraBit      // If zero then should toggle extra bit
-      lda SPRITES.EXTRA_BIT   // If non zero, check extra bit
-      and #$01
-      beq UpdateSpriteXPos    // If extra bit not set, then update position
-      cpx #$46
-      bcs CheckDirectionY     // If extra bit set and new position is over right
-      jmp UpdateSpriteXPos    // border, no movement allowed
-
-    ToggleExtraBit:
-      lda SPRITES.EXTRA_BIT
-      eor #$01
-      sta SPRITES.EXTRA_BIT
-    UpdateSpriteXPos:
-      stx SPRITES.X0
-      jmp CheckDirectionY
-
-    MoveToLeft:
-//        jsr switch_sprite_frog
-      lda SPRITES.EXTRA_BIT   // Moving to right, check extra bit
-      and #$01
-      bne TryToMoveLeft       // If extra bit is set, then move allowed
-      ldx SPRITES.X0          // Check if position is on left border
-      cpx #$16
-      bcc CheckDirectionY     // If extra bit not set and x-position is not on
-      dex                     // left border, then move allowed
-      jmp UpdateSpriteXPos
-
-    TryToMoveLeft:
-      ldx SPRITES.X0          // Calculate new position
-      dex
-      cpx #$ff
-      bne UpdateSpriteXPos    // If position is $ff then extra bit must be
-      jmp ToggleExtraBit      // toggled
-
-    CheckDirectionY:
-      lda DirectionY
-      beq CheckFire
-
-      ldy SPRITES.Y0          // Calculate new position
-
-      cmp #$ff
-      beq MoveToUp
-
-    MoveToDown:
-      iny
-      cpy #$e7
-      bcs CheckFire
-      sty SPRITES.Y0
-
-      jmp CheckFire
-
-    MoveToUp:
-      dey
-      cpy #$34
-      bcc CheckFire
-      sty SPRITES.Y0
-
-    CheckFire:
       lda FirePressed
-      beq JoystickMovement
-
+      beq CheckFire
 
     NoMovement:
       jsr Finalize
@@ -117,36 +46,12 @@ Intro: {
       lda #%00001110
       sta VIC.MEMORY_SETUP
 
-      lda #$50
-      sta SPRITE_0
-
-      lda #$fc
-      sta $d000
-      lda #$40
-      sta $d001
-
-      lda #$0a
-      sta SPRITES.EXTRACOLOR1
-
-      lda #$00
-      sta SPRITES.EXTRACOLOR2
-
-      lda #$07
-      sta SPRITES.COLOR0
-
-// Enable the first sprite (just for test)
-      lda #$01
-      sta VIC.SPRITE_ENABLE
-      sta VIC.SPRITE_MULTICOLOR
-
       rts
   }
 
   Finalize: {
-      lda #$00
-      sta VIC.SPRITE_ENABLE
-
       // Reset game var
+      lda #$00
       sta GameEnded
 
       // Reset player orientation and direction
@@ -166,7 +71,5 @@ Intro: {
 
       rts
   }
-
-  .label SPRITE_0   = $43f8
 
 }
--- level1.asm
+++ level1.asm
@@ -17,16 +17,118 @@ Level1: {
       jsr AddColorToMap
       jsr StupidWaitRoutine
 
+    JoystickMovement:
+      jsr WaitRoutine
+      jsr GetJoystickMove
+
+      lda Direction
+      beq CheckDirectionY
+
+// Handle horizontal move
+      cmp #$ff
+      beq MoveToLeft
+
+    MoveToRight:
+      jsr UpdateRangerFrame
+      ldx SPRITES.X0          // Moving to right
+      inx                     // Calculate new sprite x position
+      beq ToggleExtraBit      // If zero then should toggle extra bit
+      lda SPRITES.EXTRA_BIT   // If non zero, check extra bit
+      and #$01
+      beq UpdateSpriteXPos    // If extra bit not set, then update position
+      cpx #LIMIT_RIGHT
+      bcs CheckDirectionY     // If extra bit set and new position is over right
+      jmp UpdateSpriteXPos    // border, no movement allowed
+
+    ToggleExtraBit:
+      lda SPRITES.EXTRA_BIT
+      eor #$01
+      sta SPRITES.EXTRA_BIT
+    UpdateSpriteXPos:
+      stx SPRITES.X0
+      jmp CheckDirectionY
+
+    MoveToLeft:
+      jsr UpdateRangerFrame
+      lda SPRITES.EXTRA_BIT   // Moving to right, check extra bit
+      and #$01
+      bne TryToMoveLeft       // If extra bit is set, then move allowed
+      ldx SPRITES.X0          // Check if position is on left border
+      cpx #LIMIT_LEFT
+      bcc CheckDirectionY     // If extra bit not set and x-position is not on
+      dex                     // left border, then move allowed
+      jmp UpdateSpriteXPos
+
+    TryToMoveLeft:
+      ldx SPRITES.X0          // Calculate new position
+      dex
+      cpx #$ff
+      bne UpdateSpriteXPos    // If position is $ff then extra bit must be
+      jmp ToggleExtraBit      // toggled
+
+    CheckDirectionY:
+      lda DirectionY
+      beq CheckFirePressed
+
+      ldy SPRITES.Y0          // Calculate new position
+
+      cmp #$ff
+      beq MoveToUp
+
+    MoveToDown:
+      iny
+      cpy #LIMIT_DOWN
+      bcs CheckFirePressed
+      sty SPRITES.Y0
+
+      jmp CheckFirePressed
+
+    MoveToUp:
+      dey
+      cpy #LIMIT_UP
+      bcc CheckFirePressed
+      sty SPRITES.Y0
+
     CheckFirePressed:
-      jsr GetOnlyFirePress
-      lda #$ff
-      cmp FirePressed
-      bne CheckFirePressed
-      inc FirePressed
+      lda FirePressed
+      beq JoystickMovement
 
       jsr Finalize
 
       rts
+  }
+
+  * = * "Level1 UpdateRangerFrame"
+  UpdateRangerFrame: {
+      inc RangerFrame
+      lda RangerFrame
+      lsr
+      lsr
+      bcc CheckVerticalMove
+
+      lda Direction
+      beq CheckVerticalMove
+      cmp #$ff
+      beq Left
+
+    Right:
+      lda #RANGER_STANDING + 5
+      sta SPRITE_0
+      jmp CheckVerticalMove
+
+    Left:
+      lda #RANGER_STANDING + 7
+      sta SPRITE_0
+
+    CheckVerticalMove:
+      //lda DirectionY
+      //beq NoMove
+
+    NoMove:
+      rts
+
+    RangerFrame:
+      .byte $00
   }
 
   // Initialization of intro screen
@@ -48,10 +150,35 @@ Level1: {
       lda #%00011110
       sta VIC.MEMORY_SETUP
 
+// Init ranger
+      lda #RANGER_STANDING
+      sta SPRITE_0
+
+      lda #$50
+      sta SPRITES.X0
+      lda #$40
+      sta SPRITES.Y0
+
+      lda #$0a
+      sta SPRITES.EXTRACOLOR1
+
+      lda #$00
+      sta SPRITES.EXTRACOLOR2
+
+      lda #$07
+      sta SPRITES.COLOR0
+
+// Enable the first sprite (just for test)
+      lda #$01
+      sta VIC.SPRITE_MULTICOLOR
+      sta VIC.SPRITE_ENABLE
+
       rts
   }
 
   Finalize: {
+      lda #$00
+      sta VIC.SPRITE_ENABLE
 
       rts
   }
@@ -63,5 +190,13 @@ Level1: {
 
     rts
   }
+
+  .label SPRITE_0     = $47f8
+  .label LIMIT_UP     = $32
+  .label LIMIT_DOWN   = $e7
+  .label LIMIT_LEFT   = $16
+  .label LIMIT_RIGHT  = $46
+
+  .label RANGER_STANDING  = $50
 
 }
